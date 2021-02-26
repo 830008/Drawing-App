29991b576c0149f611f5952c47a14fc6152ecdfe
830008 <nrollins189@gmail.com>
2021-02-26 12:33:49 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let c;
+function setup() {
+let canvas = createCanvas(displayWidth, displayHeight);
+background(0)
+}
+function mouseDragged() {
+    c = color(255, 255, 255);
+    strokeWeight(5);
+let thestroke = stroke(c);
+line(mouseX, mouseY, pmouseX, pmouseY);
+
+}
+function savefunction() {
+    saveCanvas(canvas, 'MyCanvas', 'png')
+}
